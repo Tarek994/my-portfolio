3fa82be39675ae659ea9b7c9fa551cb44ba51ce9
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-15 23:59:37 +0300

    chore(App.tsx): try  demo code

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
 import './App.css';
+import Grid from '@mui/material/Grid';
+
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Grid>
+      hello world
+    </Grid>
   );
 }
 
