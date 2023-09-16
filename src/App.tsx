6cfce4fba17375c954d45c911c5fdf00b1b94ad8
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-16 23:51:48 +0300

    feat(App.tsx): prepare the App.tsx to display the components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import './App.css';
-import Grid from '@mui/material/Grid';
-
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import { styled } from "@mui/material/styles";
+import Grid from "@mui/material/Unstable_Grid2";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
+  const MainGrid = styled(Grid)(({ theme }) => ({
+    backgroundColor: `#fff`,
+  }));
+
   return (
-    <Grid>
-      hello world
-    </Grid>
+    <Router>
+      <MainGrid justifyContent="center" xs={12} container maxWidth={"100%"}>
+        <Navbar />
+        <Grid maxWidth="lg" spacing={2} xs={12} container>
+          <Routes>
+            
+          </Routes>
+        </Grid>
+      </MainGrid>
+    </Router>
   );
 }
-
 export default App;
