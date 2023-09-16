751484fe2ef3b838a6c00fe683a5bea613de3487
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-16 23:50:13 +0300

    feat(Navbar.tsx): add the navabr component to the app

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,60 @@
+import Grid from '@mui/material/Unstable_Grid2';
+import Tabs from '@mui/material/Tabs';
+import LinkTab from '@mui/material/Tab';
+import { Box } from "@mui/system";
+import { Typography } from "@mui/material";
+
+const Navbar = () => {
+    const handleChange = (e: any) => {
+        console.log(e)
+    }
+    return (
+        <Grid
+            container
+            xs={12}
+            md={12}
+            maxWidth={'lg'}
+            justifyContent="flex-end"
+            justifyItems="center"
+            alignContent="center"
+            alignItems="baseLine"
+            sx={{ p: 2 }}>
+
+            <Grid xs={6} md={2}>
+                <Box>
+                    <Typography sx={{
+
+                        fontSize: {
+                            xs: 22,
+                        },
+                        fontFamily: 'ADLaM Display',
+                        fontWeight: 800,
+                    }}>
+                        Tarek Ghali
+                    </Typography>
+                </Box>
+            </Grid>
+            <Grid
+                container
+                xs={6}
+                md={10}
+                justifyContent="flex-end"
+                justifyItems="center"
+                alignContent="center"
+            >
+                <div>
+                    <Tabs
+                        onChange={handleChange}
+                        aria-label="basic tabs example"
+                    >
+                        <LinkTab label="About" href='/#' />
+                        <LinkTab label="Services" href='/#/services' />
+                        <LinkTab label="Projects" href='/#/projects' />
+                    </Tabs>
+                </div>
+            </Grid>
+        </Grid >
+    );
+};
+
+export default Navbar;
