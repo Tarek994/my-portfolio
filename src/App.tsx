95e01043d199bcab7c8f7896dd46db5d818a0efe
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-22 23:22:58 +0300

    chore(App.tsx): add the landing page to the App file

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { styled } from "@mui/material/styles";
 import Grid from "@mui/material/Unstable_Grid2";
 import Navbar from "./components/navbar/Navbar";
+import Container from "./home/homepage/Container";
 
 function App() {
   const MainGrid = styled(Grid)(({ theme }) => ({
@@ -13,9 +14,9 @@ function App() {
     <Router>
       <MainGrid justifyContent="center" xs={12} container maxWidth={"100%"}>
         <Navbar />
-        <Grid maxWidth="lg" spacing={2} xs={12} container>
+        <Grid spacing={2} xs={12} container>
           <Routes>
-            
+            <Route path="/" element={<Container />} />
           </Routes>
         </Grid>
       </MainGrid>
