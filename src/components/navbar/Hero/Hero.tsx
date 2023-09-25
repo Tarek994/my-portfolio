a48dea6a67988cab55281c012b1ccbdb523274d3
Tarek994 <ghali.tarek.123@hotmail.com>
2023-09-25 23:02:04 +0300

    feat(Hero.tsx): add the hero section typography

--- src/components/navbar/Hero/Hero.tsx
+++ src/components/navbar/Hero/Hero.tsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import { Grid,Typography } from '@mui/material';
+
+
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <Grid container xs={12} sm={12} md={12} lg={12}
+    sx={{
+      padding:'1rem',
+      borderRadius:'15px',
+      width:'100%',
+      backgroundColor:'#fff',
+      display:'flex',
+    }}
+    >
+      <Grid container xs={12} sm={12} md={6} lg={6} sx={{display:'flex', flexDirection:'column'}}>
+          <Typography variant="h4"> Hello World I'm Tarek</Typography>
+          <Typography variant='h6'> Front End Developer</Typography>
+
+      </Grid>
+
+      <Grid container xs={12} sm={12} md={6} lg={6}>
+
+      </Grid>
+
+
+    </Grid>
   )
 }
 
