909fac5e488a32a61db9858030a704a6cd10c630
Tarek994 <ghali.tarek.123@hotmail.com>
2023-10-02 23:33:51 +0300

    feat(Services.tsx): add the fixed style for the services component

--- src/components/services/Services.tsx
+++ src/components/services/Services.tsx
@@ -1,6 +1,18 @@
 import { Grid } from '@mui/material';
 
 const Services = () => {
+    return (
+        <Grid container xs={12} sm={12} md={12} lg={12}
+            sx={{
+                padding: '1rem',
+                borderRadius: '15px',
+                width: '100%',
+                backgroundColor: '#fff',
+                display: 'flex',
+            }}>
+
+                
+        </Grid>
     )
 }
 
