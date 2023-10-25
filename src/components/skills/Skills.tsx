e71218d161863df28960f9130a39861b75273d55
Tarek994 <ghali.tarek.123@hotmail.com>
2023-10-26 02:22:38 +0300

    chore(Skills.tsx): formatting the code

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -12,66 +12,67 @@ import javascript from "./assets/javascript.png"
 const Skills = () => {
   return (
     <Grid container xs={12} sm={12} md={12} lg={12}
-    sx={{
-      padding: '1rem',
-      borderRadius: '15px',
-      width: '100%',
-      backgroundColor: '#fff',
-      display: 'flex',
-      margin:'1rem 0rem'
-    }}
-  >
-    <Grid container xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', marginTop: '1rem', padding: '1rem' }}>
+      sx={{
+        padding: '1rem',
+        borderRadius: '15px',
+        width: '100%',
+        backgroundColor: '#fff',
+        display: 'flex',
+        margin: '1rem 0rem'
+      }}
+    >
+      <Grid container xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', marginTop: '1rem', padding: '1rem' }}>
 
-    <Card sx={{ maxWidth: 250,margin:"0.5rem" }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={html}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          HTML
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Proficient in HTML for creating structured and responsive web pages.
-        </Typography>
-      </CardContent>
-    </Card>
-        <Card sx={{ maxWidth: 250,margin:"0.5rem" }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={css}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          CSS/3
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-        Proficient in CSS for designing visually appealing and responsive web interfaces.
-        </Typography>
-      </CardContent>
-    </Card>
-    <Card sx={{ maxWidth: 250,margin:"0.5rem" }}>
-      <CardMedia
-        component="img"
-        alt="green iguana"
-        height="140"
-        image={javascript}
-      />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          Javascript
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
- Proficient in using JS to enhance interactivity and functionality on web applications.        </Typography>
-      </CardContent>
-    </Card>
+        <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image={html}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              HTML
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Proficient in HTML for creating structured and responsive web pages.
+            </Typography>
+          </CardContent>
+        </Card>
+
+        <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
+          <CardMedia
+            component="img"
+            alt="CSS"
+            height="140"
+            image={css}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              CSS/3
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Proficient in CSS for designing visually appealing and responsive web interfaces.
+            </Typography>
+          </CardContent>
+        </Card>
+        <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
+          <CardMedia
+            component="img"
+            alt="green iguana"
+            height="140"
+            image={javascript}
+          />
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Javascript
+            </Typography>
+            <Typography variant="body2" color="text.secondary">
+              Proficient in using JS to enhance interactivity and functionality on web applications.        </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
     </Grid>
-  </Grid>
   )
 }
 
