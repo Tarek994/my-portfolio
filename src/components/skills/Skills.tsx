7f28902615151ced98d543d85668ad0d0324adae
Tarek994 <ghali.tarek.123@hotmail.com>
2023-12-14 07:38:34 +0200

    chore(Skills.tsx): formatting the code

--- src/components/skills/Skills.tsx
+++ src/components/skills/Skills.tsx
@@ -1,39 +1,55 @@
 import { Grid } from "@mui/material";
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
-import Typography from '@mui/material/Typography';
-import css from "./assets/css.png"
-import html from "./assets/html.png"
-import javascript from "./assets/javascript.png"
-import jquery from "./assets/jquery.jpg"
-import git from "./assets/git-github.png"
-import react from "./assets/react.png"
-import agile from "./assets/agile.png"
-import wordpress from "./assets/wordpress.png"
-import soft from "./assets/soft.jpg"
-
-
-
-
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Typography from "@mui/material/Typography";
+import css from "./assets/css.png";
+import html from "./assets/html.png";
+import javascript from "./assets/javascript.png";
+import jquery from "./assets/jquery.jpg";
+import git from "./assets/git-github.png";
+import react from "./assets/react.png";
+import agile from "./assets/agile.png";
+import wordpress from "./assets/wordpress.png";
+import soft from "./assets/soft.jpg";
 
 const Skills = () => {
   return (
-    <Grid container xs={12} sm={12} md={12} lg={12}
+    <Grid
+      container
+      xs={12}
+      sm={12}
+      md={12}
+      lg={12}
       sx={{
-        padding: '1rem',
-        borderRadius: '15px',
-        width: '100%',
-        backgroundColor: '#fff',
-        display: 'flex',
-        margin: '1rem 0rem'
+        padding: "1rem",
+        borderRadius: "15px",
+        width: "100%",
+        backgroundColor: "#fff",
+        display: "flex",
+        margin: "1rem 0rem",
       }}
     >
-      <Grid container xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', marginTop: '1rem', padding: '1rem' }}>
-        <Typography variant="h4" fontWeight={500}>Programing Skills</Typography>
+      <Grid
+        container
+        xs={12}
+        sm={12}
+        md={12}
+        lg={12}
+        sx={{ display: "flex", marginTop: "1rem", padding: "1rem" }}
+      >
+        <Typography variant="h4" fontWeight={500}>
+          Programing Skills
+        </Typography>
       </Grid>
-      <Grid container xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', margin: '1rem 0rem', padding: '1rem' }}>
-
+      <Grid
+        container
+        xs={12}
+        sm={12}
+        md={12}
+        lg={12}
+        sx={{ display: "flex", margin: "1rem 0rem", padding: "1rem" }}
+      >
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
           <CardMedia
             component="img"
@@ -46,24 +62,22 @@ const Skills = () => {
               HTML
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Proficient in HTML for creating structured and responsive web pages.
+              Proficient in HTML for creating structured and responsive web
+              pages.
             </Typography>
           </CardContent>
         </Card>
 
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
-          <CardMedia
-            component="img"
-            alt="CSS"
-            height="140"
-            image={css}
-          />
+          <CardMedia component="img" alt="CSS" height="140" image={css} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               Design and Style
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Accomplished in CSS and adept at utilizing leading design frameworks, including Bootstrap, Tailwind CSS, and Material UI, to create visually appealing and responsive.
+              Accomplished in CSS and adept at utilizing leading design
+              frameworks, including Bootstrap, Tailwind CSS, and Material UI, to
+              create visually appealing and responsive.
             </Typography>
           </CardContent>
         </Card>
@@ -79,7 +93,9 @@ const Skills = () => {
               Javascript with Frame Work
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Masterful in  <b>JavaScript</b> and <b>Typescript</b> to enhance web application interactivity.        </Typography>
+              Masterful in <b>JavaScript</b> and <b>Typescript</b> to enhance
+              web application interactivity.{" "}
+            </Typography>
           </CardContent>
         </Card>
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
@@ -94,7 +110,9 @@ const Skills = () => {
               jquery
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Skilled in utilizing jQuery to enhance interactivity and functionality on web applications.        </Typography>
+              Skilled in utilizing jQuery to enhance interactivity and
+              functionality on web applications.{" "}
+            </Typography>
           </CardContent>
         </Card>
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
@@ -109,7 +127,9 @@ const Skills = () => {
               ReactJS
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Proficient in <b>ReactJS</b> for dynamic user interfaces and experienced in state management.        </Typography>
+              Proficient in <b>ReactJS</b> for dynamic user interfaces and
+              experienced in state management.{" "}
+            </Typography>
           </CardContent>
         </Card>
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
@@ -124,50 +144,58 @@ const Skills = () => {
               CMS Software
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Skilled in <b>WordPress</b>, specializing in effective content management and dynamic website creation.        </Typography>
+              Skilled in <b>WordPress</b>, specializing in effective content
+              management and dynamic website creation.{" "}
+            </Typography>
           </CardContent>
         </Card>
       </Grid>
 
       {/* ANOTHER SKILLS */}
-      <Grid container xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', marginTop: '1rem', padding: '1rem' }}>
-        <Typography variant="h4" fontWeight={500}>Another Skills</Typography>
+      <Grid
+        container
+        xs={12}
+        sm={12}
+        md={12}
+        lg={12}
+        sx={{ display: "flex", marginTop: "1rem", padding: "1rem" }}
+      >
+        <Typography variant="h4" fontWeight={500}>
+          Another Skills
+        </Typography>
       </Grid>
 
-
-
-      <Grid container xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', margin: '1rem 0rem', padding: '1rem' }}>
-
-
+      <Grid
+        container
+        xs={12}
+        sm={12}
+        md={12}
+        lg={12}
+        sx={{ display: "flex", margin: "1rem 0rem", padding: "1rem" }}
+      >
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
-          <CardMedia
-            component="img"
-            alt="green iguana"
-            height="140"
-            image={git}
-          />
+          <CardMedia component="img" alt="git" height="140" image={git} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               Version Control
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Experienced in leveraging Git and GitHub to streamline collaborative development processes.
+              Experienced in leveraging Git and GitHub to streamline
+              collaborative development processes.
             </Typography>
           </CardContent>
         </Card>
         <Card sx={{ maxWidth: 250, margin: "0.5rem" }}>
-          <CardMedia
-            component="img"
-            alt="CSS"
-            height="140"
-            image={agile}
-          />
+          <CardMedia component="img" alt="agile" height="140" image={agile} />
           <CardContent>
             <Typography gutterBottom variant="h5" component="div">
               Project Management
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Experienced in <b>Agile</b> project management with a focus on <b>Jira</b> and <b>GitHub</b> for efficient task tracking, issue resolution, and streamlined collaboration within software development teams.
+              Experienced in <b>Agile</b> project management with a focus on{" "}
+              <b>Jira</b> and <b>GitHub</b> for efficient task tracking, issue
+              resolution, and streamlined collaboration within software
+              development teams.
             </Typography>
           </CardContent>
         </Card>
@@ -183,14 +211,15 @@ const Skills = () => {
               Soft Skills
             </Typography>
             <Typography variant="body2" color="text.secondary">
-              Versatile professional with strong soft skills, adept in Office applications, ERP systems, and effective communication. Proven ability to seamlessly navigate diverse tasks, fostering collaboration and enhancing operational efficiency.
+              Versatile professional with strong soft skills, adept in Office
+              applications, ERP systems, and effective communication. Proven
+              ability to seamlessly navigate diverse tasks, fostering
+              collaboration and enhancing operational efficiency.
             </Typography>
           </CardContent>
         </Card>
-
-
       </Grid>
     </Grid>
-  )
-}
-export default Skills
+  );
+};
+export default Skills;
