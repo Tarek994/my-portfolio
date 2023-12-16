2347741445ba66a572f91386b2097b47bc968432
Tarek994 <ghali.tarek.123@hotmail.com>
2023-12-16 21:44:14 +0200

    style(Container.tsx): change the bg-color

--- src/home/homepage/Container.tsx
+++ src/home/homepage/Container.tsx
@@ -7,7 +7,7 @@ import Skills from "../../components/skills/Skills"
 const Container = () => {
   return (
     <Grid container xs={12} md={12} lg={12}
-      sx={{ backgroundColor: '#caf0f8', padding: '1rem 0rem' }}
+      sx={{ backgroundColor: '#003049', padding: '1rem 0rem' }}
     >
       <Grid container xs={11} sm={11} md={11} lg={11}
         sx={{ maxWidth: 'lg', margin: 'auto' }}
