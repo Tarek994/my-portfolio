754d5cc376e3ba397f548a47c3f0c11340b08934
Tarek994 <ghali.tarek.123@hotmail.com>
2023-12-17 21:41:31 +0200

    feat(Hero.tsx): add a button for the experience section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,6 +1,6 @@
 import { Grid, Typography } from '@mui/material';
 import Button from '@mui/material/Button';
-import hero from './assets/hero.svg'
+import hero from './assets/hero.svg';
 
 
 const Hero = () => {
@@ -18,7 +18,10 @@ const Hero = () => {
       <Grid container xs={12} sm={12} md={12} lg={6} sx={{ display: 'flex', flexDirection: 'column', marginTop: '1rem', padding: '1rem' }}>
         <Typography variant="h4">Front End Developer</Typography>
         <Typography variant='h6' sx={{ marginTop: '1rem' }}>Dedicated web enthusiast and adaptable collaborator, with over 18 months of expertise primarily in Front-end and JavaScript domains. Committed to fostering personal and team development, supported by a history of achievements.#WebDevelopment #Teamwork. </Typography>
-        <Grid><Button variant="contained" sx={{ marginTop: '1rem' }}>Projects</Button></Grid>
+        <Grid sx={{display:"flex"}}>
+          <Button variant="contained" sx={{ marginTop: '1rem', marginRight: "5px" }}>Projects</Button>
+          <Button variant="contained" sx={{ marginTop: '1rem', marginRight: "5px" }}>Experience</Button>
+        </Grid>
       </Grid>
 
       <Grid container xs={12} sm={12} md={12} lg={6} sx={{ display: 'flex', marginTop: '1rem', padding: '1rem' }}>
